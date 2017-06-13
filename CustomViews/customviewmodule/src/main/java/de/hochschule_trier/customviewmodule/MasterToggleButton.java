dcf539b3fff9fe4363c65c07eaf8767ce551f1fd
ChristianHarten <christianharten23@gmail.com>
2017-06-13 18:47:27 +0200

    Teilaufgabe 2 disableSlaves Variante

--- CustomViews/customviewmodule/src/main/java/de/hochschule_trier/customviewmodule/MasterToggleButton.java
+++ CustomViews/customviewmodule/src/main/java/de/hochschule_trier/customviewmodule/MasterToggleButton.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
