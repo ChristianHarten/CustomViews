f87f124f1d7a5c5e21dbe00120bc0c16184e2d37
ChristianHarten <christianharten23@gmail.com>
2017-06-13 18:18:09 +0200

    Teilaufgabe 2 disableSlaves Variante

--- CustomViews/customviewmodule/src/main/java/de/hochschule_trier/customviewmodule/MasterToggleButton.java
+++ CustomViews/customviewmodule/src/main/java/de/hochschule_trier/customviewmodule/MasterToggleButton.java
@@ -1,6 +1,7 @@
 package de.hochschule_trier.customviewmodule;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.Toast;
@@ -11,16 +12,20 @@ import java.util.List;
 
 public class MasterToggleButton extends ToggleButton{
     private List<ToggleButton> slaves;
+    private boolean disableSlaves;
     public MasterToggleButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        parseAttributes(attrs);
         init();
     }
     public MasterToggleButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        parseAttributes(attrs);
         init();
     }
     public MasterToggleButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        parseAttributes(attrs);
         init();
     }
     public MasterToggleButton(Context context) {
@@ -28,10 +33,12 @@ public class MasterToggleButton extends ToggleButton{
         init();
     }
     public void addSlaves(ToggleButton slave) {
+        slave.setClickable(!this.disableSlaves);
         this.slaves.add(slave);
     }
     public void addSlaves(ToggleButton[] newSlaves) {
         for (ToggleButton slave : newSlaves) {
+            slave.setClickable(!this.disableSlaves);
             this.slaves.add(slave);
         }
     }
@@ -55,6 +62,25 @@ public class MasterToggleButton extends ToggleButton{
     }
     public List<ToggleButton> getSlaves() {
         return this.slaves;
+    }
+    public boolean getDisableSlaves() {
+        return this.disableSlaves;
+    }
+    public void setDisableSlaves(boolean value) {
+        this.disableSlaves = value;
+        refreshSlaveList();
+    }
+    private void refreshSlaveList() {
+        if (!this.slaves.isEmpty()) {
+            for (ToggleButton slave : slaves) {
+                slave.setClickable(!this.disableSlaves);
+            }
+        }
+    }
+    private void parseAttributes(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MasterToggleButton);
+        disableSlaves = a.getBoolean(R.styleable.MasterToggleButton_disableSlaves, disableSlaves);
+        a.recycle();
     }
     private void handleEvent(ToggleButton slave) {
         if (slave instanceof MasterToggleButton && ((MasterToggleButton) slave).hasSlaves()) {
